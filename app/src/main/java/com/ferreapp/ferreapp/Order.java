5734cf51100de9c9d23033637a4ad1df25442e05
Baptiste <baptiste.viry@orange.fr>
2018-10-08 16:05:28 -0500

    adding Product and Order class
    with getter,setter,constructors

---
+++ app/src/main/java/com/ferreapp/ferreapp/Order.java
@@ -0,0 +1,90 @@
+package com.ferreapp.ferreapp;
+
+import java.util.Arrays;
+
+public class Order {
+
+    private String seller;
+    private String buyer;
+    private int totalPrice;
+    private String state; //1-pagado 2-por_pagar 3-entregago 4-rechazado
+    private String comment; //bueno, deficiente, excelente, regular
+    private String commentDescription;
+    private Product[] products;
+
+    public Order() {
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setCommentDescription(String commentDescription) {
+        this.commentDescription = commentDescription;
+    }
+
+    public String getSeller() {
+
+        return seller;
+    }
+
+    public String getBuyer() {
+        return buyer;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getCommentDescription() {
+        return commentDescription;
+    }
+
+    public Product[] getProducts() {
+        return products;
+    }
+
+    public Order(String seller, String buyer, int totalPrice, String state, String comment, String commentDescription, Product[] products) {
+
+        this.seller = seller;
+        this.buyer = buyer;
+        this.totalPrice = totalPrice;
+        this.state = state;
+        this.comment = comment;
+        this.commentDescription = commentDescription;
+        this.products = products;
+    }
+
+    public Order(String seller, String buyer, int totalPrice, Product[] products) {
+
+        this.seller = seller;
+        this.buyer = buyer;
+        this.totalPrice = totalPrice;
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "seller='" + seller + '\'' +
+                ", buyer='" + buyer + '\'' +
+                ", totalPrice=" + totalPrice +
+                ", state='" + state + '\'' +
+                ", comment='" + comment + '\'' +
+                ", commentDescription='" + commentDescription + '\'' +
+                ", products=" + Arrays.toString(products) +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/ferreapp/ferreapp/Product.java
@@ -0,0 +1,52 @@
+package com.ferreapp.ferreapp;
+
+public class Product {
+
+    private String name;
+    private String category;
+    private int price;
+    private String description;
+    private String brand;
+
+    public Product(String name, String category, int price, String description, String brand){
+        this.name=name;
+        this.category=category;
+        this.price=price;
+        this.description=description;
+        this.brand=brand;
+    }
+
+    public Product() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
+}
