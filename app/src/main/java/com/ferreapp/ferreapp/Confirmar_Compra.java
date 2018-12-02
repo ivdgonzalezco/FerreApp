a76e488d8e6f036d5d2e428cc47480a891b3662f
misael1986 <gabelogan0167@hotmail.com>
2018-12-02 14:13:06 -0500

    Version más revisada de las vitas y procesos de Compras y recibo de pago

--- app/src/main/java/com/ferreapp/ferreapp/Confirmar_Compra.java
+++ app/src/main/java/com/ferreapp/ferreapp/Confirmar_Compra.java
@@ -1,20 +1,104 @@
 package com.ferreapp.ferreapp;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class Confirmar_Compra extends AppCompatActivity {
 
+    private String[][] carrito = {
+            {"Tornillos", "100", "10", "1000"},
+            {"Chasos", "100", "10", "1000"},
+            {"Broca 1/2", "4500", "20", "9000"},
+            {"Pintura", "23000", "1", "23000"}
+    };
+    String Total = "";
 
+    private TableLayout tablapos;
+    private int ntabla = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.confirmar_compra);
-
+        tablapos = (TableLayout) findViewById(R.id.tablaCompras);
+        llenar_tabla();
+        calc_total();
     }
 
     public void comprar(View view) {
+
+        Intent compra=new Intent(this,Pagos.class);
+        compra.putExtra("ValorCompra",Total);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("carrito", carrito);
+        compra.putExtras(bundle);
+        startActivity(compra);
+    }
+
+
+    public void llenar_tabla() {
+        if (tablapos.getChildCount() > 1) {
+            Log.i("fslog", "nrows=" + tablapos.getChildCount());
+            int filas = tablapos.getChildCount();
+            tablapos.removeViews(1, filas - 1);
+            for (int i = 0; i < carrito[i].length; i++) {
+                Log.i("fslog", "matriz[i].length=" + carrito[i].length);
+                TableRow fila = new TableRow(this);
+                fila.setBackgroundColor(Color.parseColor("#FFFFFF"));
+
+                TextView tv1 = new TextView(this);
+                TextView tv2 = new TextView(this);
+                TextView tv3 = new TextView(this);
+                TextView tv4 = new TextView(this);
+
+                tv1.setText(carrito[0][i]);
+                tv2.setText(carrito[1][i]);
+                tv3.setText(carrito[2][i]);
+                tv4.setText(carrito[3][i]);
+
+                tablapos.addView(fila);
+            }
+
+        } else {
+            for (int i = 0; i < carrito[i].length; i++) {
+                Log.i("fslog", "matriz[i].length=" + carrito[i].length);
+                TableRow fila = new TableRow(this);
+                fila.setBackgroundColor(Color.parseColor("#FFFFFF"));
+
+                TextView tv1 = new TextView(this);
+                TextView tv2 = new TextView(this);
+                TextView tv3 = new TextView(this);
+                TextView tv4 = new TextView(this);
+
+                tv1.setText(carrito[0][i]);
+                tv2.setText(carrito[1][i]);
+                tv3.setText(carrito[2][i]);
+                tv4.setText(carrito[3][i]);
+
+                tablapos.addView(fila);
+
+            }
+
+        }
+
+    }
+
+    public void calc_total() {
+        double suma = 0;
+
+        for (int i = 0; i < carrito[i].length; i++) {
+            suma = suma + Double.parseDouble(carrito[3][i]);
+
+        }
+        Total=""+suma;
+        TextView tot= (TextView) findViewById(R.id.totalPagar);
+        tot.setText("Total a Pagar: $"+ Total);
     }
 }
---
+++ app/src/main/java/com/ferreapp/ferreapp/Pagos.java
@@ -0,0 +1,84 @@
+package com.ferreapp.ferreapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.graphics.pdf.PdfDocument;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+public class Pagos extends AppCompatActivity {
+
+    String Total;
+    List<String> carrito;
+    Bundle extras;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pagos);
+        extras = getIntent().getExtras();
+        Total=extras.getString("Total");
+        Intent este = this.getIntent();
+        Bundle bundle = este.getExtras();
+        carrito= (List<String>) bundle.getSerializable("carrito");
+
+
+    }
+
+    public String Generar_factura(){
+
+        PdfDocument document = new PdfDocument();
+
+        // repaint the user's text into the page
+        View content = findViewById(R.id.tablaCompras);
+
+        // crate a page description
+        int pageNumber = 1;
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(content.getWidth(),
+                content.getHeight() - 20, pageNumber).create();
+
+        // create a new page from the PageInfo
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        content.draw(page.getCanvas());
+
+        // do final processing of the page
+        document.finishPage(page);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyyhhmmss");
+        String pdfName = "pdfdemo"
+                + sdf.format(Calendar.getInstance().getTime()) + ".pdf";
+
+        File outputFile = new File("/sdcard/FerreApp/", pdfName);
+
+        try {
+            outputFile.createNewFile();
+            OutputStream out = new FileOutputStream(outputFile);
+            document.writeTo(out);
+            document.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return outputFile.getPath();
+
+    }
+
+    public void generar(View view) {
+
+        String mensaje= Generar_factura();
+        Toast.makeText(this, mensaje,Toast.LENGTH_LONG);
+    }
+}
+
+
